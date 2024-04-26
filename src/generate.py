d6a22839ecdc31235d832c20b920691146474c9f
maikdevries <dev.maik.devries@outlook.com>
2024-04-26 15:00:29 +0200

    feat: generator functions for sets X, Y and W

---
+++ src/generate.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+def zero_X (T, N):
+	"""
+	Generate a T-by-N boolean matrix which contains T N-dimensional zero vectors.
+	"""
+	return np.zeros((T, N), dtype = bool)
+
+
+def random_X (T, N):
+	"""
+	Generate a T-by-N boolean matrix which contains T N-dimensional vectors with a single random entry set to True.
+	"""
+
+	# Generate T random indices between 0 and N (exclusive)
+	random_indices = np.random.randint(0, N, size = T)
+
+	# Generate T-by-N zero matrix and set a random element of each vector to True
+	X = zero_X(T, N)
+	X[np.arange(T), random_indices] = True
+
+	return X
+
+
+def random_bounded_X (T, N, B):
+	"""
+	Generate a T-by-N boolean matrix which contains T N-dimensional vectors with a single random entry in range [0 .. B] set to True.
+	"""
+
+	# Generate T random indices between 0 and B (exclusive)
+	random_indices = np.random.randint(0, B, size = T)
+
+	# Generate T-by-N zero matrix and set random element of each vector to True
+	X = zero_X(T, N)
+	X[np.arange(T), random_indices] = True
+
+	return X
+
+
+def zero_Y (N):
+	"""
+	Generate an N-dimensional zero vector.
+	"""
+	return np.zeros(N, dtype = np.float64)
+
+
+def random_Y (N, C):
+	"""
+	Generate an N-dimensional vector with each element in range [0 .. 1] and all elements sum up to at most C.
+	"""
+
+	# Generate N-dimensional vector with its elements randomly set in range [0 .. 1]
+	Y = np.random.rand(N)
+
+	# Scale Y such that all elements sum up to at most C
+	Y *= (C / np.sum(Y))
+
+	return Y
+
+
+def uniform_weights (N):
+	"""
+	Generate an N-dimensional boolean vector with each element set to True.
+	"""
+	return np.ones(N, dtype = bool)
+
+
+def random_weights (N):
+	"""
+	Generate an N-dimensional floating-point vector with each element in range [0 .. 1].
+	"""
+	return np.random.rand(N)
