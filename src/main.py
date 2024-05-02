fe51cd76b0961559b9f8d7c73902b8d9b1accf06
maikdevries <dev.maik.devries@outlook.com>
2024-05-02 12:55:41 +0200

    feat: add LRU policy to comparison plots

--- src/main.py
+++ src/main.py
@@ -27,11 +27,15 @@ if __name__ == '__main__':
 
 	# Retrieve lists of utility progression over time for various caching policies
 	BSH_utility = benchmark.calc_utility_BSH(X, W, C)
+	LRU_utility = benchmark.calc_utility_LRU(X, W, C)
 	OGA_utility = benchmark.calc_utility_OGA(X, W, T, N, C, R)
 
 	print(f'Utility accumulated by BSH policy: {BSH_utility[-1]}')
+	print(f'Utility accumulated by LRU policy: {LRU_utility[-1]}')
 	print(f'Utility accumulated by OGA policy: {OGA_utility[-1]}')
-	print(f'Regret achieved by OGA policy: {(BSH_utility[-1] - OGA_utility[-1])}')
+
+	print(f'Regret achieved by OGA vs BSH policy: {(BSH_utility[-1] - OGA_utility[-1])}')
+	print(f'Regret achieved by OGA vs LRU policy: {(LRU_utility[-1] - OGA_utility[-1])}')
 
 	(fig, (dist, util)) = plt.subplots(2, 1)
 	fig.suptitle(f'Average request utility over time [N = {N}, C = {C}, R = {R}]')
@@ -48,6 +52,7 @@ if __name__ == '__main__':
 	time_slots = np.arange(1, T + 1)
 
 	util.plot((BSH_utility / time_slots), label = 'BSH')
+	util.plot((LRU_utility / time_slots), label = 'LRU')
 	util.plot((OGA_utility / time_slots), label = 'OGA')
 
 	plt.legend()
