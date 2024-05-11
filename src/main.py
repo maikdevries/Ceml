6c6b6922a3f58168f15c8b391152d29ce03858b0
maikdevries <dev.maik.devries@outlook.com>
2024-05-11 17:45:58 +0200

    refactor: move parameters into separate file

--- src/main.py
+++ src/main.py
@@ -2,32 +2,8 @@ import concurrent.futures
 import numpy as np
 import matplotlib.pyplot as plt
 
-import generate
+from parameters import T, N, C, X, W, R
 import benchmark
-
-
-# The number of time slots, and consequently the number of requests or iterations (horizon)
-T = 50000
-
-# The size of the system's library of files and cache, where C should be (significantly) smaller than N
-N = 5000
-C = 500
-
-# T-by-N matrix: at each timeslot t a single entry is set to True (file request)
-X = generate.random_X_zipfian(T, N, alpha = 0.8)
-
-# N-dimensional vector: static file-dependent utility vector (utility weights)
-W = generate.uniform_weights(N)
-
-# Learning rates to be used in online gradient ascent algorithm (computed dynamically for None entries)
-R = [0.05, 0.1, 0.3, 0.5, 0.7, 1.0]
-
-
-# Assert that the input parameters are within valid ranges
-assert T > 0, 'The number of time slots T must be positive'
-assert N > 0, 'The system library size N must be positive'
-assert C > 0 and C < N, 'The cache size C must be within range [1 .. N - 1]'
-assert R, 'The list of learning rates R must not be empty'
 
 
 if __name__ == '__main__':
