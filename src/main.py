0478d2455d3d555f18fe967ef65a3da8023165a5
maikdevries <dev.maik.devries@outlook.com>
2024-05-13 13:45:12 +0200

    refactor: move meta learner to separate module

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import concurrent.futures
-import numpy as np
 
-from parameters import T, N, C, X, W, R, L
+from parameters import T, N, C, X, W, R
 import benchmark
 import output
 
@@ -18,18 +17,12 @@ if __name__ == '__main__':
 		LRU_utility, LRU_caches, LRU_time = LRU_future.result()
 		OGA_utilities, OGA_caches, OGA_times = zip(*[future.result() for future in OGA_futures])
 
-	# Calculate the utility progression over time for the meta learner
-	EG_utility, EG_weights, EG_time = benchmark.calc_utility_EG(np.asarray(OGA_utilities).T, len(R), L)
-
 	# Print the running time of each caching policy to the console
 	print(f'[{BSH_time:.2f}s] BSH cache policy')
 	print(f'[{LRU_time:.2f}s] LRU cache policy')
 
 	for i, r in enumerate(R):
 		print(f'[{OGA_times[i]:.2f}s] OGA [{r}] cache policy')
-
-	# Print the running time of the meta learner to the console
-	print(f'[{EG_time:.2f}s] EG [{L}] meta learner')
 
 	# Save the generated request matrix (X) to disk
 	output.save_request_matrix(X, 'request_matrix')
@@ -40,6 +33,3 @@ if __name__ == '__main__':
 
 	for i, r in enumerate(R):
 		output.save_results(OGA_utilities[i], OGA_caches[i], f'OGA_[{r}]')
-
-	# Save the utility progression and weights states of the meta learner to disk
-	output.save_results(EG_utility, EG_weights, f'EG_[{L}]')
---
+++ src/meta_learner.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from parameters import R, L
+import benchmark
+import output
+
+
+if __name__ == '__main__':
+
+	# Load the utility progression of each caching policy expert from disk
+	OGA_utilities, _ = zip(*[output.load_results(f'OGA_[{r}]') for r in R])
+
+	# Calculate the utility progression over time for the meta learner
+	EG_utility, EG_weights, EG_time = benchmark.calc_utility_EG(np.asarray(OGA_utilities).T, len(R), L)
+
+	# Print the running time of the meta learner to the console
+	print(f'[{EG_time:.2f}s] EG [{L}] meta learner')
+
+	# Save the utility progression and weights states of the meta learner to disk
+	output.save_results(EG_utility, EG_weights, f'EG_[{L}]')
